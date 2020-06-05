a6f77c733f526156bc4199a63461cf76cb3b8de7
Yuliya Mihaylova <yulu.2000@hotmail.com>
2020-06-06 00:27:11 +0300

    01

---
+++ decision_tree_tegressor.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+
+# show table 
+melbourne_data.describe()
+
+# show columns
+melbourne_data.columns
+
+# dropna drops missing values (think of na as "not available")
+melbourne_data = melbourne_data.dropna(axis=0)
+
+# column
+y = melbourne_data.Price
+
+# features
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+
+# top
+X.head()
+
+from sklearn.tree import DecisionTreeRegressor
+
+# Define model. Specify a number for random_state to ensure same results each run
+melbourne_model = DecisionTreeRegressor(max_leaf_nodes=500, random_state=1)
+
+# Fit model
+melbourne_model.fit(X, y)
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+predicted = melbourne_model.predict(X)
+print(melbourne_model.predict(X.head()))
+
+from sklearn.metrics import mean_absolute_error
+
+print(f"Mean absolute error = {mean_absolute_error(y, predicted)}")
+
+from sklearn.model_selection import train_test_split
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+
+
+####################### search best n-leafe
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+min_mae, leafe_nodes = get_mae(candidate_max_leaf_nodes[0], train_X, val_X, train_y, val_y), candidate_max_leaf_nodes[0]
+for leaf in candidate_max_leaf_nodes[1:]:
+    mae = get_mae(leaf,train_X, val_X, train_y, val_y)
+    if mae < min_mae:
+        min_mae, leafe_nodes = mae, leaf
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = leafe_nodes
+
+########################
