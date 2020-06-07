aa3776fcf8ebcf1e5e86a6cbbf24c394f80c1c2f
Yuliya Mihaylova <yulu.2000@hotmail.com>
2020-06-08 02:57:23 +0300

    02

--- decision_tree_tegressor.py
+++ decision_tree_tegressor.py
@@ -67,3 +67,11 @@ for leaf in candidate_max_leaf_nodes[1:]:
 best_tree_size = leafe_nodes
 
 ########################
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
