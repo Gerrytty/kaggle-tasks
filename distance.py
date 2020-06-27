6ac86d79181cc66bad12997daa63283b7854a54d
Yuliya Mihaylova <yulu.2000@hotmail.com>
2020-06-28 01:53:54 +0300

    distance functions

--- distance.py
+++ distance.py
@@ -1,6 +1,7 @@
 import math
 
 
+# Manhattan distance
 def l1(data1, data2):
     distance = 0
 
@@ -10,15 +11,22 @@ def l1(data1, data2):
     return distance
 
 
+# Euclidean distance
 def l2(data1, data2):
     distance = 0
 
     for i1, i2 in data1, data2:
-        r = i1 - i2
-        distance += r * r
+        diff = i1 - i2
+        distance += diff * diff
 
     return math.sqrt(distance)
 
 
-def loss():
-    pass
+# data should be without of correct label
+def loss(data, w_of_correct_label):
+    L = 0
+
+    for w_of_labels in data:
+        L += max(0, w_of_labels - w_of_correct_label + 1)
+
+    return L
